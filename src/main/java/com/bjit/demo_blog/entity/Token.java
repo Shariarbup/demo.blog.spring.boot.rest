eff0670d206c710ef446e7b2001afcedddd43dfa
Al Shariar <35493040+Shariarbup@users.noreply.github.com>
2023-12-04 20:54:43 +0600

    added token entity

---
+++ src/main/java/com/bjit/demo_blog/entity/Token.java
@@ -0,0 +1,27 @@
+package com.bjit.demo_blog.entity;
+
+import com.bjit.demo_blog.enums.TokenType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tokens")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    private TokenType tokenType;
+
+    private boolean expired;
+
+    private boolean revoked;
+}
