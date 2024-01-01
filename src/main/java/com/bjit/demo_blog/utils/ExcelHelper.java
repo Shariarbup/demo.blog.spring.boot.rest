b6da1b56ef20b115d4b08ca8d125e7cfdfbc8af0
Al Shariar <35493040+Shariarbup@users.noreply.github.com>
2024-01-01 22:05:34 +0600

    Update

--- src/main/java/com/bjit/demo_blog/utils/ExcelHelper.java
+++ src/main/java/com/bjit/demo_blog/utils/ExcelHelper.java
@@ -9,18 +9,18 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class ExcelHelper {
 
+    Logger logger = LoggerFactory.getLogger(ExcelHelper.class);
     public static String[] HEADERS={
             "ID",
             "ABOUT",
@@ -130,9 +130,22 @@ public class ExcelHelper {
     private List<User> parseCSVFile(final MultipartFile file) throws Exception {
         final List<User> users = new ArrayList<>();
         try {
-            try {
-                
+            try (final BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    final String[] data = line.split(",");
+                    final User user = new User();
+                    user.setName(data[1]);
+                    user.setEmail(data[2]);
+                    user.setPassword(data[3]);
+                    user.setAbout(data[4]);
+                    users.add(user);
+                }
+                return users;
             }
-        } catch (Exception e) {}
+        } catch (final IOException e) {
+            logger.error("Failed to parse CSV file {}", e);
+            throw new Exception("Failed to parse CSV file {}", e);
+        }
     }
 }
